f55381615a31e45ec5b4550f86886a87dfc03d1b
mxwiser <neo@qvqol.com>
2026-07-22 15:36:37 +0800

    fix fifo

--- udp_18k/src/udp.sv
+++ udp_18k/src/udp.sv
@@ -191,17 +191,19 @@ always @(posedge clk50m or negedge phy_rdy) begin
             tick <= tick + 8'd1;
             if(tick == 3)tick <= 0;
         end
-        rx_cnt <= 0;
+        
         fifo_drop <= 1'b0;
 
         case(rx_state)
             0:begin
                 rx_state <= 1;
+                rx_cnt <= 0;
             end
             1:begin //检测前导码和相位 / Detect preamble and alignment phase
                 if(rx_data_s[7:0] == 8'h55)begin
                     rx_state <= 2;
                 end
+                rx_cnt <= 0;
             end
             2:begin
                 tick <= 1;
@@ -303,9 +305,9 @@ task rx_head_fifo_push(input [31:0] data);
         rx_head_fifo_head_int <= 0;
 endtask
 
-shortint rx_data_fifo_head_int;
-shortint rx_data_fifo_head;
-shortint rx_data_fifo_tail = 0;
+logic [31:0] rx_data_fifo_head_int;
+logic [31:0] rx_data_fifo_head;
+logic [31:0] rx_data_fifo_tail = 0;
 logic [7:0] rx_data_fifo[8191:0];
 
 logic [7:0] rx_data_fifo_i_port;
@@ -418,7 +420,7 @@ always_ff@(posedge clk50m or negedge phy_rdy)begin
             20:begin
                 //如果fifo满了，就直接拒绝接收 / Reject reception when FIFO is full
                 //head 剩余空间小于4 或者 data 剩余空间小于1600 / head free space < 4 or data free space < 1600
-                if((rx_data_fifo_tail + 127 - rx_data_fifo_head_int) % 128 < 4)
+                if((rx_head_fifo_tail + 127 - rx_head_fifo_head_int) % 128 < 4)
                     ethernet_resolve_status <= 100;
                 if((rx_data_fifo_tail + 8191 - rx_data_fifo_head_int) % 8192 < 1600)
                     ethernet_resolve_status <= 100;
@@ -619,21 +621,22 @@ always_ff@(posedge clk50m or negedge phy_rdy)begin
         rx_head_av_o<=1'b0;
         rx_data_av_o<=1'b0;
     end else begin
-        rx_head_av_o <= rx_head_fifo_head != rx_head_fifo_tail;
-        if(read_head)rx_head_av_o <= rx_head_fifo_head != (rx_head_fifo_tail+1)%128;
-
-        if(read_head)rx_head_fifo_tail <= (rx_head_fifo_tail+1)%16'd128;
-
-        rx_head_o <= rx_head_fifo[rx_head_fifo_tail];
-        if(read_head) rx_head_o <= rx_head_fifo[(rx_head_fifo_tail+1)%128];
-
-        rx_data_av_o <= rx_data_fifo_head != rx_data_fifo_tail;
-        if(read_data)rx_data_av_o <= rx_data_fifo_head != (rx_data_fifo_tail+1)%8192;
-
-        if(read_data)rx_data_fifo_tail <= (rx_data_fifo_tail+1)%16'd8192;
-
-        rx_data_o <= rx_data_fifo[rx_data_fifo_tail];
-        if(read_data) rx_data_o <= rx_data_fifo[(rx_data_fifo_tail+1)%8192];
+        if(read_head) begin 
+            rx_head_av_o <= rx_head_fifo_head != (rx_head_fifo_tail+1)%128;
+            rx_head_fifo_tail <= (rx_head_fifo_tail+1)%16'd128;
+            rx_head_o <= rx_head_fifo[(rx_head_fifo_tail+1)%128];
+        end else begin
+            rx_head_av_o <= rx_head_fifo_head != rx_head_fifo_tail;
+            rx_head_o <= rx_head_fifo[rx_head_fifo_tail];
+        end
+        if(read_data) begin
+            rx_data_av_o <= rx_data_fifo_head != (rx_data_fifo_tail+1)%8192;
+            rx_data_fifo_tail <= (rx_data_fifo_tail+1)%16'd8192;
+            rx_data_o <= rx_data_fifo[(rx_data_fifo_tail+1)%8192];
+        end else begin
+            rx_data_av_o <= rx_data_fifo_head != rx_data_fifo_tail;
+            rx_data_o <= rx_data_fifo[rx_data_fifo_tail];
+        end
     end
 end
 
